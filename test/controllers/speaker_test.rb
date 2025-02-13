3b0d4bd3b2c2aaf8b1d860d1d27824dfef2c676c
andycorrales111 <andycorrales@tamu.edu>
2025-02-13 17:28:55 +0000

    Speaker model test

--- test/controllers/speaker_test.rb
+++ test/controllers/speaker_test.rb
@@ -8,6 +8,18 @@ class SpeakerTest < ActiveSupport::TestCase
   end
   test "should not save speaker without email" do
     speaker = Speaker.new
-    speaker.name = "Test, "
-    assert_not speaker.save
+    speaker.name = "Test, Test"
+    assert_not speaker.save, "Saved the speaker without an email"
+  end
+  test "should not save speaker with invalid email" do
+    speaker = Speaker.new
+    speaker.name = "Test, Test"
+    speaker.email = "invalid_email!com"
+    assert_not speaker.save, "Saved the speaker with an invalid email"
+  end
+  test "should save speaker with valid name and email" do
+    speaker = Speaker.new
+    speaker.name = "Andy, Test"
+    speaker.email = "andycorrales7@gmail.com"
+    assert speaker.save, "Did not save the speaker with a valid name and email"
 end
