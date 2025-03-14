a5af81a68569101f3b01596420bac7ec2fd4c979
Hunter Han <hunter.han@gmail.com>
2025-03-14 03:51:34 +0000

    speakers crud

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -2,12 +2,53 @@
 
 class SpeakersController < ApplicationController
   before_action :authenticate_speaker!
+  before_action :set_speaker, only: %i[show edit update destroy]
+
   def index
-    @speakers = Speaker.all # Assuming you have a Speaker model
+    @speakers = Speaker.all
   end
 
   def show
-    @speaker = Speaker.find(params[:id]) # For showing individual speaker
+    redirect_to speakers_path
+  end
+
+  def new
+    @speaker = Speaker.new
+  end
+
+  def edit
+  end
+
+  def create
+    @speaker = Speaker.new(speaker_params)
+    if @speaker.save
+      redirect_to speakers_path, notice: 'Speaker was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @speaker.update(speaker_params)
+      redirect_to speakers_path, notice: 'Speaker was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @speaker.destroy
+    redirect_to speakers_url, notice: 'Speaker was successfully destroyed.'
+  end
+
+     private
+
+  def set_speaker
+    @speaker = Speaker.find(params[:id])
+  end
+
+  def speaker_params
+    params.require(:speaker).permit(:name, :email, :details)
   end
 
   def authenticate_speaker!
